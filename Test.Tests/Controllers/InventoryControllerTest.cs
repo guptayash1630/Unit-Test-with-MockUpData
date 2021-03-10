80145fb7a54167520178112cd6e7c49fc3b8e767
YashGupta <guptayash1630@gmail.com>
2021-03-10 16:47:45 +0530

    Unit Test of controller with mockup data

---
+++ Test.Tests/Controllers/InventoryControllerTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test;
+using Test.Controllers;
+using Moq;
+
+namespace Test.Tests.Controllers
+{
+    [TestClass]
+    public class InventoryControllerTest
+    {
+        #region Inventory
+        [TestMethod]
+        public void Inventory_InventoryExists()
+        {
+            string productID = "Item1";
+            int quantity = 2;
+            // Arrange
+            InventoryController controller = new InventoryController();
+
+            // Act
+            bool result = controller.CheckInventory(productID, quantity);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Inventory_InventoryNotExists()
+        {
+            string productID = "ABCTest";
+            int quantity = 2;
+            // Arrange
+            InventoryController controller = new InventoryController();
+
+            // Act
+            bool result = controller.CheckInventory(productID, quantity);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Inventory_MockData()
+        {
+            string productID = "Item1";
+            int quantity = 2;
+            Exception ex = new Exception("An exception has occurred");
+            //var mockFRepository = new Mock<IFundsRepository>();
+            InventoryController inventory = new InventoryController();
+            List<dynamic> inventoryList = new List<dynamic>()
+            {
+               new
+               {
+                    ItemID=1,
+                    ItemName="Item1",
+                    Description="ABC test item"
+                }
+            };
+
+            bool result = inventory.CheckInventory(productID, quantity);
+            Assert.IsTrue(result);
+
+            //another way
+            //Mock<InventoryController> name = new Mock<InventoryController>();
+            //name.CallBase = true;
+            //name.Setup(x => x.CheckInventory(productID, quantity)).Returns(true);
+
+            //var results = name.Object.ChargePayment(It.IsAny<string>(), It.IsAny<int>());
+
+            //string expected = string.Format("hello {0}", results);
+
+            //Assert.AreEqual(expected, results);
+        }
+        #endregion
+
+        #region Payment
+        [TestMethod]
+        public void Payment_CreditCardExists()
+        {
+            string cardDetails = "4565123478650092";
+            // Arrange
+            InventoryController controller = new InventoryController();
+
+            // Act
+            bool result = controller.ChargePayment(cardDetails, decimal.Parse("2.03"));
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Payment_CreditCardNotExists()
+        {
+            string cardDetails = "CardTest";
+            // Arrange
+            InventoryController controller = new InventoryController();
+
+            // Act
+            bool result = controller.ChargePayment(cardDetails, decimal.Parse("2.03"));
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+        #endregion
+    }
+}
---
+++ Test/Controllers/InventoryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class InventoryController : Controller
+    {
+        List<string> inventory = new List<string>() {
+           "Item1",
+           "Item2",
+           "Item3",
+           "Item4"
+        };
+        List<string> cardDetail = new List<string>() {
+           "4565123478650092",
+           "3465123478650092",
+           "8465123478650092",
+           "4567800478650092"
+        };
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public bool CheckInventory(string productId, int qty)
+        {
+            bool quantity = false;
+            if (inventory.Contains(productId) == true)
+            {
+                quantity = true;
+            }
+            if (inventory.Contains(productId) == true && qty > 0)
+            {
+                //passing number credit card number and amount here directly
+                ChargePayment("ABC", decimal.Parse("100.01"));
+            }
+            return quantity;
+        }
+        public bool ChargePayment(string creditCardNumber, decimal amount)
+        {
+            bool isPayment = false;
+            if (cardDetail.Contains(creditCardNumber) == true)
+            {
+                isPayment = true;
+            }
+
+            return isPayment;
+        }
+    }
+}
